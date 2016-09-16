707f053b6b4e54eaf7acfd63b409a4f7002ec46f
manok5757 <manok5757@HRH-IC0027323.wrdsb.ca>
2016-09-16 09:46:26 -0400

    A1Q2

--- src/A1Q1.java
+++ src/A1Q1.java
@@ -9,13 +9,13 @@ import becker.robots.Wall;
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author manok5757
  */
 public class A1Q1 {
 //Make a program to move robot around walls
+
     /**
      * @param args the command line arguments
      */
@@ -23,16 +23,16 @@ public class A1Q1 {
         //make city
         City km = new City();
         //create a robot
-        Robot hat = new Robot(km,0,2,Direction.WEST);
-         //make wall inbetween hat's movement CHANGE LOCATION
-        new Wall (km,1,2,Direction.EAST);
-        new Wall (km,2,2,Direction.EAST);
-        new Wall (km,1,2,Direction.NORTH);
-        new Wall (km,1,1,Direction.NORTH);
-        new Wall (km,2,2,Direction.SOUTH);
-        new Wall (km,2,1,Direction.SOUTH);
-        new Wall (km,1,1,Direction.WEST);
-        new Wall (km,2,1,Direction.WEST);
+        Robot hat = new Robot(km, 0, 2, Direction.WEST);
+        //make wall inbetween hat's movement CHANGE LOCATION
+        new Wall(km, 1, 2, Direction.EAST);
+        new Wall(km, 2, 2, Direction.EAST);
+        new Wall(km, 1, 2, Direction.NORTH);
+        new Wall(km, 1, 1, Direction.NORTH);
+        new Wall(km, 2, 2, Direction.SOUTH);
+        new Wall(km, 2, 1, Direction.SOUTH);
+        new Wall(km, 1, 1, Direction.WEST);
+        new Wall(km, 2, 1, Direction.WEST);
         //get bob to move
         hat.move();
         hat.move();
@@ -50,20 +50,20 @@ public class A1Q1 {
         hat.move();
         hat.turnLeft();
         hat.move();
-      
-        
-        
-        
-        
-        
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
---
+++ src/A1Q3.java
@@ -0,0 +1,32 @@
+
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.Robot;
+import becker.robots.Wall;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author manok5757
+ */
+public class A1Q3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //create a city
+        City km = new City();
+        //create mountain with walls
+        new Wall
+        
+        
+        
+        // create a robot
+        Robot karel = new Robot (km,3,0,Direction.EAST);
+        
+}}
