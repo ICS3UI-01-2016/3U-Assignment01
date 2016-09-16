ecd17e932f5d1c796345acf3b827549dc12630ff
bulka4927 <bulka4927@HRH-IC0027338.wrdsb.ca>
2016-09-16 10:11:27 -0400

    Question 2 unfinished

---
+++ src/A1Q2.java
@@ -0,0 +1,32 @@
+
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.Wall;
+import becker.robots.Robot;
+import becker.robots.Thing;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author bulka4927
+ */
+public class A1Q2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    //create a city
+    City lon = new City();
+    //create a robot
+    Robot dart = new Robot (lon,1,2,Direction.SOUTH);
+    
+    
+    
+        // TODO code application logic here
+    }
+}
