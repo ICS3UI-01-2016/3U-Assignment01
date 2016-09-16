1017c3296116fbae7bf81f3c3a1894f823807b36
manok5757 <manok5757@HRH-IC0027323.wrdsb.ca>
2016-09-16 10:10:59 -0400

    A1Q2

--- src/A1Q3.java
+++ src/A1Q3.java
@@ -2,6 +2,7 @@
 import becker.robots.City;
 import becker.robots.Direction;
 import becker.robots.Robot;
+import becker.robots.Thing;
 import becker.robots.Wall;
 
 /*
@@ -22,11 +23,39 @@ public class A1Q3 {
         //create a city
         City km = new City();
         //create mountain with walls
-        new Wall
-        
-        
-        
+        new Wall (km,3,2,Direction.WEST);
+        new Wall (km,3,2,Direction.NORTH);
+        new Wall (km,2,3,Direction.WEST);
+        new Wall (km,1,3,Direction.WEST);
+        new Wall (km,1,3,Direction.NORTH);
+        new Wall (km,1,3,Direction.EAST);
+        new Wall (km,2,4,Direction.NORTH);
+        new Wall (km,2,4,Direction.EAST);
+        new Wall (km,3,4,Direction.EAST);
+        // place flag on ground
+        new Thing (km,3,1);
         // create a robot
         Robot karel = new Robot (km,3,0,Direction.EAST);
+        // make karel pick up a flag
+        karel.move();
+        karel.pickThing();
+        // make karel navigate mountain
+        karel.turnLeft();
+        karel.move();
+        karel.turnLeft();
+        karel.turnLeft();
+        karel.turnLeft();
+        karel.move();
+        karel.turnLeft();
+        karel.move();
+        karel.move();
+        karel.turnLeft();
+        karel.turnLeft();
+        karel.turnLeft();
+        karel.move();
+        // make karel plant flag on top of mountain
+        karel.putThin
+        
+        
         
 }}
