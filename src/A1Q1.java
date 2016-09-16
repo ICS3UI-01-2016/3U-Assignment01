ce82cc24f63c25fd4b877061ab588516259e7651
gavra1870 <gavra1870@HRH-IC0027342.wrdsb.ca>
2016-09-16 09:54:58 -0400

    Offical Assigment 1

---
+++ src/A1Q1.java
@@ -0,0 +1,25 @@
+
+import becker.robots.City;
+import becker.robots.Direction;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author gavra1870
+ */
+public class A1Q1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Create City for robot
+        City Na = new City();
+        Robot Steve = new Robot(Na, 3, 4, Direction. WEST);
+        
+    }
+}
