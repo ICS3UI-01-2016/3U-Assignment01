72328a9f21653014751637cf56ae5117c985f122
bulka4927 <bulka4927@HRH-IC0027338.wrdsb.ca>
2016-09-19 09:35:32 -0400

    Question 2 finished

--- src/A1Q2.java
+++ src/A1Q2.java
@@ -24,9 +24,53 @@ public class A1Q2 {
     City lon = new City();
     //create a robot
     Robot dart = new Robot (lon,1,2,Direction.SOUTH);
+        
+            // TODO code application logic here
+    
+    //placing walls
+    new Wall(lon,1,1,Direction.WEST);
+    new Wall(lon,2,1,Direction.WEST);
+    new Wall(lon,1,1,Direction.NORTH);
+    new Wall(lon,1,2,Direction.NORTH);
+    new Wall(lon,1,2,Direction.EAST);
+    new Wall(lon,1,2,Direction.SOUTH);
+    
+    //place thing
+    new Thing(lon,2,2);
+    
+    
+    //move the robot
+    dart.turnLeft();
+    dart.turnLeft();
+    dart.turnLeft();
+    dart.move();
+    dart.turnLeft();
+    dart.move();
+    dart.turnLeft();
+    dart.move();
+    
+    //retrieving the thing
+    dart.pickThing();
+    dart.turnLeft();
+    dart.turnLeft();
+    dart.move();
+    dart.turnLeft();
+    dart.turnLeft();
+    dart.turnLeft();
+    dart.move();
+    dart.turnLeft();
+    dart.turnLeft();
+    dart.turnLeft();
+    dart.move();
+    dart.turnLeft();
+    dart.turnLeft();
+    dart.turnLeft();
     
     
     
-        // TODO code application logic here
-    }
+    
+    
+    
+    
+       }
 }
