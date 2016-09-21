84232a49b8263d1cd84ec2760e78a652ee351fec
Candice <Candice@192.168.0.12>
2016-09-20 22:14:52 -0400

    a1q3 fin.

--- src/a1q3.java
+++ src/a1q3.java
@@ -2,6 +2,7 @@
 import becker.robots.City;
 import becker.robots.Direction;
 import becker.robots.Robot;
+import becker.robots.Thing;
 import becker.robots.Wall;
 import java.awt.Color;
 
@@ -28,6 +29,48 @@ public class a1q3 {
             //change color of robot
             candice.setColor(Color.black);
             //create a wall
-            new Wall(mtl,3,3,Direction.WEST);
+            new Wall(mtl,3,2,Direction.WEST);
+            new Wall(mtl,2,3,Direction.WEST);
+            new Wall(mtl,1,3,Direction.WEST);
+            new Wall(mtl,1,3,Direction.EAST);
+            new Wall(mtl,1,3,Direction.NORTH);
+            new Wall(mtl,3,2,Direction.NORTH);
+            new Wall(mtl,2,4,Direction.NORTH);
+            new Wall(mtl,2,4,Direction.EAST);
+            new Wall(mtl,3,4,Direction.EAST);
+            //place flag (thing)
+            new Thing(mtl,3,1);
+            //get candice to climb mountain with flag
+            candice.move();
+            candice.pickThing();
+            candice.turnLeft();
+            candice.move();
+            candice.turnLeft();
+            candice.turnLeft();
+            candice.turnLeft();
+            candice.move();
+            candice.turnLeft();
+            candice.move();
+            candice.move();
+            candice.turnLeft();
+            candice.turnLeft();
+            candice.turnLeft();
+            candice.move();
+            candice.putThing();
+            //get candice to go down the mountain
+            candice.move();
+            candice.turnLeft();
+            candice.turnLeft();
+            candice.turnLeft();
+            candice.move();
+            candice.turnLeft();
+            candice.move();
+            candice.turnLeft();
+            candice.turnLeft();
+            candice.turnLeft();
+            candice.move();
+            candice.move();
+            candice.turnLeft();
+            
     }
 }
