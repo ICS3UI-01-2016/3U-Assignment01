cd4773092542c5a0d05c38847497605a2cad19e0
Madi <Madi@192.168.0.11>
2016-09-21 14:17:02 -0400

    Completed Assignment 1.

--- src/A1Q5.java
+++ src/A1Q5.java
@@ -1,3 +1,12 @@
+
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.Robot;
+import becker.robots.RobotSE;
+import becker.robots.Thing;
+import becker.robots.Wall;
+import java.awt.Color;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -13,7 +22,51 @@ public class A1Q5 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // Create city
-          
+
+        City Q5 = new City();
+        
+        new Wall(Q5,2,3,Direction.NORTH);
+        new Wall(Q5,2,3,Direction.EAST);
+        new Wall(Q5,2,3,Direction.WEST);
+        new Wall(Q5,3,3,Direction.EAST);
+        new Wall(Q5,3,3,Direction.SOUTH);
+        
+        Thing one = new Thing(Q5,0,0);
+        one.setColor(Color.BLACK);
+        Thing two = new Thing(Q5,1,0);
+        two.setColor(Color.BLACK);
+        Thing three = new Thing(Q5,1,1);
+        three.setColor(Color.BLACK);
+        Thing four = new Thing(Q5,1,2);
+        four.setColor(Color.BLACK);
+        Thing five = new Thing(Q5,2,2);
+        five.setColor(Color.BLACK);
+        
+        RobotSE rick = new RobotSE(Q5,3,3,Direction.EAST);
+        rick.setColor(Color.RED);
+        rick.setLabel("R");
+        
+        RobotSE bob = new RobotSE(Q5,0,1,Direction.WEST);
+        bob.setColor(Color.RED);
+        bob.setLabel("B");
+        
+        bob.move();
+        bob.pickThing();
+        rick.turnLeft();
+        rick.turnLeft();
+        rick.move();
+        bob.turnLeft();
+        rick.turnRight();
+        bob.move();
+        bob.pickThing();
+        rick.move();
+        rick.pickThing();
+        bob.turnLeft();
+        rick.move();
+        bob.move();
+        bob.pickThing();
+        rick.pickThing();
+        rick.turnLeft();
+  
     }
 }
