9df0c3cde63c14ad90c8d45a007197732693634b
DhanaAnvic <fabed2976@googleapps.wrdsb.ca>
2016-09-20 20:33:17 -0400

    Update A1Q4.java

--- src/A1Q4.java
+++ src/A1Q4.java
@@ -13,6 +13,31 @@ public class A1Q4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+         City SK = new City();
+         Robot Chen = new Robot (SK, 0,0, Direction. SOUTH);
+         Robot Suho = new Robot (SK, 0,1, Direction. SOUTH);
+         
+         new Wall (SK, 0,1, Direction. WEST);
+         new Wall (SK, 1,1, Direction. WEST);
+         new Wall (SK, 1,1, Direction. SOUTH);
+         
+         Chen. move();
+         Chen. move();
+         Chen. turnLeft();
+         Chen.move();
+         
+         Suho. move();
+         Suho. turnLeft();
+         Suho. move();
+         Suho. turnLeft();
+         Suho. turnLeft();
+         Suho. turnLeft();
+         Suho. move();
+         Suho. turnLeft();
+         Suho. turnLeft();
+         Suho. turnLeft();
+         Suho. move();
+         
     }
 }
