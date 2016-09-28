ae97477f200e42c79d6a190dcb86d1893bd9c70a
slatz8075 <slatz8075@HRH-IC0027337.wrdsb.ca>
2016-09-28 09:37:33 -0400

    Added my name to the file

--- src/A1Q3.java
+++ src/A1Q3.java
@@ -2,6 +2,7 @@
 import becker.robots.City;
 import becker.robots.Direction;
 import becker.robots.Robot;
+import becker.robots.Thing;
 import becker.robots.Wall;
 
 /*
@@ -22,14 +23,48 @@ public class A1Q3 {
         // Create a city for the robot
         City A1Q3 = new City();
         //Create a Robot
-        Robot Jeff = new Robot(A1Q3,0,3,Direction.EAST);
+        Robot Jeff = new Robot(A1Q3,3,0,Direction.EAST);
         //Create 
         new Wall(A1Q3,3,2,Direction.WEST);
-        new Wall(A1Q3,3,3,Direction.NORTH);
-        new Wall(A1Q3,1,1,Direction.WEST);
-        new Wall(A1Q3,2,1,Direction.WEST);
-        new Wall(A1Q3,1,2,Direction.EAST);
-        new Wall(A1Q3,2,1,Direction.SOUTH);
-        new Wall(A1Q3,1,2,Direction.SOUTH);
+        new Wall(A1Q3,3,2,Direction.NORTH);
+        new Wall(A1Q3,2,3,Direction.WEST);
+        new Wall(A1Q3,1,3,Direction.WEST);
+        new Wall(A1Q3,1,3,Direction.EAST);
+        new Wall(A1Q3,1,3,Direction.NORTH);
+        new Wall(A1Q3,2,4,Direction.NORTH);
+        new Wall(A1Q3,2,4,Direction.EAST);
+        new Wall(A1Q3,3,4,Direction.EAST);
+        
+        new Thing(A1Q3,3,1);
+        
+        Jeff.move();
+        Jeff.pickThing();        
+        Jeff.turnLeft();
+        Jeff.move();
+        Jeff.turnLeft();
+        Jeff.turnLeft();
+        Jeff.turnLeft();
+        Jeff.move();
+        Jeff.turnLeft();
+        Jeff.move();
+        Jeff.move();
+        Jeff.turnLeft();
+        Jeff.turnLeft();
+        Jeff.turnLeft();
+        Jeff.move();
+        Jeff.putThing();
+        Jeff.move();
+        Jeff.turnLeft();
+        Jeff.turnLeft();
+        Jeff.turnLeft();
+        Jeff.move();
+        Jeff.turnLeft();
+        Jeff.move();
+        Jeff.turnLeft();
+        Jeff.turnLeft();
+        Jeff.turnLeft();
+        Jeff.move();
+        Jeff.move();
+        Jeff.turnLeft();
     }
 }
